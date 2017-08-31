b486e94b01858e06007ae00e57d8da3024d7ac0a
Rajat Choudhury <choudhuryrajat@gmail.com>
2017-08-31 12:09:05 +0530

    Test cases for GET /todos:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectId(),
     text: 'First test Todo'
 }, {
+    _id: new ObjectId(),
     text: 'Second test Todo'
 }]
 
@@ -71,3 +74,32 @@ describe('GET /todo', () => {
             .end(done);
     });
 });
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var id = new ObjectId();
+
+        request(app)
+            .get(`/todos/${id.toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
+
