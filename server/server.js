e25981180bdb97e7fd72e947b7070c5a154fc50a
Rajat Choudhury <choudhuryrajat@gmail.com>
2017-09-04 09:14:01 +0530

    Test DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -47,19 +47,19 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.delete('/todos/:id', (request, response) => {
+app.delete('/todos/:id', (request, res) => {
     var id = request.params.id;
 
     if(!ObjectID.isValid(id)){
-        return response.status(404).send();
+        return res.status(404).send();
     }
 
     Todo.findByIdAndRemove(id).then((todo) => {
         if(!todo) {
-            return request.statusCode(404).send();
+            return res.status(404).send();
         }
-        response.send({todo});
-    }).catch((e) => response.send(400).send());
+        res.send({todo});
+    }).catch((e) => res.send(400).send());
 });
 
 app.listen(port, () => {
